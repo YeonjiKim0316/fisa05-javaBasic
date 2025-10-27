900289d11711037dabbf9252956adcf01c1c398b
YeonjiKim0316 <kimyeonji3@gmail.com>
2025-10-27 12:59:55 +0900

    finish paymentEx

--- src/paymentEx/Main.java
+++ src/paymentEx/Main.java
@@ -4,8 +4,12 @@ package paymentEx;
 public class Main {
     public static void main(String[] args) {
         // 신용카드와 NaverPay 결제 객체 생성
-        Payment creditCard = new CreditCardPayment(50000);
+
+        // List list1 = new ArrayList
+        Payment creditCard = new CreditCardPayment(50000); // 업캐스팅
         Payment naverPay = new NaverPayPayment(30000);
+
+//        naverPay.getPoint(); // 는 자식클래스 NaverPayPayment로만 사용가능
 
         // 결제 처리 (정적 메서드 사용)
         PaymentProcessor.process(creditCard);
@@ -17,11 +21,13 @@ public class Main {
         // instanceof 연산자는 객체가 null인 경우 false를 반환합니다.
         // instanceof 연산자를 사용하여 타입을 확인한 후, 안전하게 다운캐스팅할 수 있습니다.
         if (creditCard instanceof Refundable) {
-            ((Refundable) creditCard).refund();
+            ((Refundable) creditCard).refund(); // (int) str
         }
 
         if (naverPay instanceof Refundable) {
             ((Refundable) naverPay).refund();
         }
+
+        // naverPay 로 얼마, credit카드로 얼마
     }
 }
--- src/paymentEx/PaymentProcessor.java
+++ src/paymentEx/PaymentProcessor.java
@@ -4,6 +4,7 @@ public class PaymentProcessor {
 
     // 클래스 메서드로 선언
     static void process(Payment payment) {
-        
+        payment.displayAmount(); // 결제금액 보여주고
+        payment.processPayment(); // 실제 결제 진행하는 순서
     }
 }
